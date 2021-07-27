f83127a436fd034908ae7eef0b480a5f0da58fe5
benNjogu <kamandemachafi.gmail.com>
2021-07-28 00:12:17 +0300

    local variables in if blocks

--- IfKeyWordAndCodeBlocks/src/com/keytech/Main.java
+++ IfKeyWordAndCodeBlocks/src/com/keytech/Main.java
@@ -5,19 +5,26 @@ public class Main {
 	public static void main(String[] args) {
 		
 		boolean gameOver = true;
-		int score = 5000;
+		int score = 800;
 		int levelCompleted = 5;
 		int bonus = 100;
 		
-		if(score <= 5000 && score > 1000) {
-			System.out.println("Your score was better");
-		}else if (score<1000) {
-			System.out.println("score too low");
-		}
-		else {
-			System.out.println("reached here");
-		}
+//		if(score <= 5000 && score > 1000) {
+//			System.out.println("Your score was better");
+//		}else if (score<1000) {
+//			System.out.println("score too low");
+//		}
+//		else {
+//			System.out.println("reached here");
+//		}
 		
+		if (gameOver) {
+			int finalScore = score + (levelCompleted*bonus);
+			System.out.println("Your final score was "+finalScore);
+		}
+		finalScore = 40;/*In this case you cannot access 
+		variables created in the if block because they are 
+		deleted by the computer after it has executed the block*/
 	}
 	
 }
