82baeb14ceb30ab727d0b73960caf38e77e6f0ad
benNjogu <kamandemachafi.gmail.com>
2021-07-27 22:17:15 +0300

    white spaces

---
+++ StatementsWhiteSpacesAndIdentation/src/com/keytech/Main.java
@@ -0,0 +1,19 @@
+package com.keytech;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * The example below works but it is not recommended to write like this
+		 * */
+		int           myVariable 
+		
+		= 
+		50
+		
+		;
+
+	}
+
+}
