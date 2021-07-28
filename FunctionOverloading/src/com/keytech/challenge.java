97084ebe1f3500eab1040aca5b2602f6cb4b55bb
benNjogu <kamandemachafi.gmail.com>
2021-07-28 12:02:00 +0300

    The correct way

--- FunctionOverloading/src/com/keytech/challenge.java
+++ FunctionOverloading/src/com/keytech/challenge.java
@@ -3,42 +3,39 @@ package com.keytech;
 public class challenge {
 
 	public static void main(String[] args) {
-		double inches = 10d;
-		double feet = calcFeetAndInchesToCentimeters(inches);
-		System.out.println(feet);
-		double cm = calcFeetAndInchesToCentimeters(feet, inches);
-		System.out.println(cm);
+		
+//		double centimeters = calcFeetAndInchesToCentimeters(10, -1);
+//		if (centimeters<0.0) {
+//			System.out.println("Invalid parameters");
+//		}
+		
+		calcFeetAndInchesToCentimeters(100);
 	}
+		
 	
 	public static double calcFeetAndInchesToCentimeters (double feet, double inches) {
-		if(feet>=0) {
-			//1 foot = 12inches
-			//convert inches to cm
-			double foot = 2.54*12;
-			double cm = foot*feet;
-			return cm;
-		}
-		if(inches>=0 && inches<=12) {
-			//1 inch = 2.54cm
-			double inch = 2.54;
-			double cm = inch*inches;
-			return cm;
-		}
-		else {
+		//1 foot = 12inches
+		//1 inch = 2.54cm
+		
+		if((feet<0) || (inches<0) || (inches>12)) {
+			System.out.println("Invalid feet or inches parameters");
 			return -1;
 		}
+		
+		double centimetres = (feet*12)*2.54;
+		centimetres += inches*2.54;
+		System.out.println(feet+"feet "+inches+"inches = "+centimetres+"cm.");
+		return centimetres;
 	}
 	
 	public static double calcFeetAndInchesToCentimeters (double inches) {
-		
-		if(inches>=0 && inches<=12) {
-			//1 foot = 12inches
-			double inch = 12;
-			double foot = inches/inch;
-			return foot;
-		}
-		else {
+		if(inches<0) {
 			return -1;
 		}
+		
+		double feet = (int) inches/12;
+		double remainingInches = (int) inches%12;
+		System.out.println(inches +"inches is equal to "+feet+"feet and"+remainingInches+" inches");
+		return calcFeetAndInchesToCentimeters(feet, remainingInches);
 	}
 }
