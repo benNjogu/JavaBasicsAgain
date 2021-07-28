e3ff34e6d4c687d16fa9ce36930b3badb8108156
benNjogu <kamandemachafi.gmail.com>
2021-07-28 08:58:12 +0300

    with parameters

--- Methods/src/com/keytech/Main.java
+++ Methods/src/com/keytech/Main.java
@@ -4,16 +4,16 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		calculateScore();
+		calculateScore(10000, 8, 200);
 		
 	}
 	
-	public static void calculateScore() {
-		int score = 1000;
-		int levelCompleted = 8;
-		int bonus = 200;
+	public static void calculateScore(int score, int level, int bonus) {
+//		int score = 1000;
+//		int levelCompleted = 8;
+//		int bonus = 200;
 		
-		int finalScore = score + (levelCompleted*bonus);
+		int finalScore = score + (level*bonus);
 		System.out.println(finalScore);
 	}
 }
