4b94bc56e82aeec990c0c743c0fd92b9ddc258ea
benNjogu <kamandemachafi.gmail.com>
2021-07-31 16:14:12 +0300

    challenge2(string) - with toLowerCase

--- SwitchStatement/src/com/keytech/Challenge.java
+++ SwitchStatement/src/com/keytech/Challenge.java
@@ -5,16 +5,36 @@ public class Challenge {
 	public static void main(String[] args) {
 
 		char grade = 'A';
-		switch(grade) {
-		
-		case 'A': case 'B': case 'C': case 'D': case 'E':
-		System.out.println("Your grade is "+grade);
-		break;
+		switch (grade) {
+
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E':
+			System.out.println("Your grade is " + grade);
+			break;
 		default:
 			System.out.println("Seems like you never took your exam, child.");
 			break;
 		}
-		
+
 	}
 
+	public static class ChallengeWithString {
+		public static void main(String[] args) {
+			String month = "JAnuAry";
+			switch (month.toLowerCase()) {
+			case "january":
+				System.out.println("Jan");
+				break;
+			case "june":
+				System.out.println("hune");
+				break;
+			default:
+				System.out.println("guess again!");
+				break;
+			}
+		}
+	}
 }
