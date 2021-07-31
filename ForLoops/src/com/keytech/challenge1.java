0accbbfc01555a380c73740dca084d36d73951d4
benNjogu <kamandemachafi.gmail.com>
2021-07-31 17:13:53 +0300

    adding 2 decimals using string format

--- ForLoops/src/com/keytech/challenge1.java
+++ ForLoops/src/com/keytech/challenge1.java
@@ -4,7 +4,7 @@ public class challenge1 {
 
 	public static void main(String[] args) {
 		for (int i = 2; i <= 8; i++) {
-			System.out.println("10,000 at " + i + "% interest = " + calculateInterest(10000, i));
+			System.out.println("10,000 at " + i + "% interest = "+String.format("%.2f", calculateInterest(10000, i)));
 		}
 	}
 
