23b70eae75fd2bf145a0f169da2c2290ecc131fa
benNjogu <kamandemachafi.gmail.com>
2021-07-31 16:51:48 +0300

    many calculations

--- ForLoops/src/com/keytech/Main.java
+++ ForLoops/src/com/keytech/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("10,000 at 2% interest = "+calculateInterest(10000, 2));
+		System.out.println("10,000 at 3% interest = "+calculateInterest(10000, 3));
+		System.out.println("10,000 at 4% interest = "+calculateInterest(10000, 4));
+		System.out.println("10,000 at 5% interest = "+calculateInterest(10000, 5));
+		System.out.println("10,000 at 6% interest = "+calculateInterest(10000, 6));
 		
 	}
 	
