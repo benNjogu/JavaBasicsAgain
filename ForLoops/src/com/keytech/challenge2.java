f50bd627bed22d4c96936e23effa413a802b45d2
benNjogu <kamandemachafi.gmail.com>
2021-07-31 17:17:28 +0300

    challenge2-for loop backwards

---
+++ ForLoops/src/com/keytech/challenge2.java
@@ -0,0 +1,19 @@
+package com.keytech;
+
+public class challenge2 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 8; i >= 2; i--) {
+			System.out.println("10,000 at " + i + "% interest = "+String.format("%.2f", calculateInterest(10000, i)));
+		}
+		
+	}
+	
+	public static double calculateInterest(double amount, double interestRate) {
+
+		return (amount * (interestRate / 100));
+
+	}
+	
+}
