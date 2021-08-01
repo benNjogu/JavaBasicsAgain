aa288535e0ae13d39842ad384779abb45d4b3ea1
benNjogu <kamandemachafi.gmail.com>
2021-08-01 14:40:57 +0300

    while loop with break statement

--- WhileAndDoWhile/src/com/keytech/Main.java
+++ WhileAndDoWhile/src/com/keytech/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		int count=0;
-		while(count != 5) {
+		while(count != 6) {
+			System.out.println("Count value is "+count);
+			count++;
+		}
+		System.out.println("****************");
+		count = 1;
+		while(true) {
+			if(count==6) {
+
+				break;
+			}
 			System.out.println("Count value is "+count);
 			count++;
 		}
