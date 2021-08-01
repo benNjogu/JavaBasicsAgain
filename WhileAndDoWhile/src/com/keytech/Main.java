9e8db8d01f1f8544d038d526c479988524249f65
benNjogu <kamandemachafi.gmail.com>
2021-08-01 13:56:23 +0300

    while loop

---
+++ WhileAndDoWhile/src/com/keytech/Main.java
@@ -0,0 +1,13 @@
+package com.keytech;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int count=0;
+		while(count != 5) {
+			System.out.println("Count value is "+count);
+			count++;
+		}
+	}
+	
+}
