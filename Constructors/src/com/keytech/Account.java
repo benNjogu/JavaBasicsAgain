ffaea993c2928386fae6f944dc2a256e8eafe4b3
benNjogu <kamandemachafi.gmail.com>
2021-08-02 12:28:38 +0300

    empty constructor

---
+++ Constructors/src/com/keytech/Account.java
@@ -0,0 +1,74 @@
+package com.keytech;
+
+public class Account {
+
+	private int accNo;
+	private double balance;
+	private String customerName, email, phoneNumber;
+	
+	public Account() {
+	
+		System.out.println("Empty constructor called!");
+		
+	}
+
+	public int getAccNo() {
+		return accNo;
+	}
+
+	public void setAccNo(int accNo) {
+		this.accNo = accNo;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void deposit(double amount) {
+		this.balance += amount;
+		System.out.println("You have deposited " + amount + " on your account.\nYour balance is " + this.balance);
+	}
+
+	public void withdraw(double amount) {
+		if (this.balance < 0) {
+			System.out.println("You've got no money...");
+		} else if (this.balance - amount <= 0) {
+			System.out.println("Not enough money!! Your balance is "+this.balance);
+		} else if (this.balance < amount) {
+			System.out.println("You do not have enough money.\nYour balance is " + this.balance);
+		} else {
+			this.balance -= amount;
+			System.out
+					.println(amount + " successfully withdrawn from your account. Account balance is " + this.balance);
+		}
+	}
+
+}
---
+++ Constructors/src/com/keytech/Main.java
@@ -0,0 +1,11 @@
+package com.keytech;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Account bensAcct = new Account();
+		bensAcct.deposit(1000);
+		bensAcct.withdraw(999);
+	}
+	
+}
