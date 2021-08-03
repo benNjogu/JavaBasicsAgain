96bfed0b121ed08f84361c1eb0114b3c09ab5bb2
benNjogu <kamandemachafi.gmail.com>
2021-08-03 11:09:09 +0300

    Inheritance part 1

---
+++ Inheriance/src/com/keytech/Main.java
@@ -0,0 +1,12 @@
+package com.keytech;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Animal animal = new Animal("mammal", 1, 1, 5, 5);
+		Dog dog = new Dog("pom", 2, 12, 2, 4, 1, 42, "hairy");
+		dog.eat();
+	}
+	
+}
