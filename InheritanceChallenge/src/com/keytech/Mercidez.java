d1da28dbd6c1a7c452de2f6d390a328ee9ad8502
benNjogu <kamandemachafi.gmail.com>
2021-08-05 23:53:57 +0300

    challenge part 1

---
+++ InheritanceChallenge/src/com/keytech/Mercidez.java
@@ -0,0 +1,14 @@
+package com.keytech;
+
+public class Mercidez extends Car{
+
+	private int roadServiceMonths;
+
+	public Mercidez(int roadServiceMonths) {
+		super("Mercidez", "SUV", 5, 5, 6, false);
+		this.roadServiceMonths = roadServiceMonths;
+	}
+	
+	
+	
+}
