da21717b1a8cc2c7d4bf657b5fbe77633d7eb2ff
benNjogu <kamandemachafi.gmail.com>
2021-08-10 18:31:23 +0300

    with encapsulation

--- Encapsulation/src/com/keytech/Main.java
+++ Encapsulation/src/com/keytech/Main.java
@@ -3,19 +3,24 @@ package com.keytech;
 public class Main {
 
 	public static void main(String[] args) {
-		Player player = new Player();
-		player.name = "Ben";
-		player.health = 20;
-		player.weapon = "Knife";
-		int damage = 10;
+//		Player player = new Player();
+//		player.name = "Ben";
+//		player.health = 20;
+//		player.weapon = "Knife";
+//		int damage = 10;
+//		
+//		player.loseHealth(damage);
+//		System.out.println("Remaining health = "+player.healthRemaining());
+//		
+//		damage = 11;
+//		player.loseHealth(damage);
+//		System.out.println("Remaining health = "+player.healthRemaining());
 		
-		player.loseHealth(damage);
-		System.out.println("Remaining health = "+player.healthRemaining());
-		
-		damage = 11;
-		player.loseHealth(damage);
-		System.out.println("Remaining health = "+player.healthRemaining());
-		
+
+		EnhancedPlayer player = new EnhancedPlayer("Ben", 1, "AK47");
+		player.loseHealth(5);
+		System.out.println(player.getHealth());
+	
 	}
 	
 }
