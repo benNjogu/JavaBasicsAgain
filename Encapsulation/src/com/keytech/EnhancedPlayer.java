9f11c7c3fd5777c1504d908a72f74d4ef5a2b51b
benNjogu <kamandemachafi.gmail.com>
2021-08-10 18:39:38 +0300

    Refactoring in a constructor

--- Encapsulation/src/com/keytech/EnhancedPlayer.java
+++ Encapsulation/src/com/keytech/EnhancedPlayer.java
@@ -3,28 +3,28 @@ package com.keytech;
 public class EnhancedPlayer {
 
 	private String name;
-	private int health = 100;
+	private int hitPoints = 100;
 	private String weapon;
 
 	public EnhancedPlayer(String name, int health, String weapon) {
 		super();
 		this.name = name;
 		if(health>0 && health<=100) {
-			this.health = health;
+			this.hitPoints = health;
 		}
 		this.weapon = weapon;
 	}
 
 	public void loseHealth(int damage) {
-		this.health -= damage;
-		if(this.health <=0) {
+		this.hitPoints -= damage;
+		if(this.hitPoints <=0) {
 			System.out.println("Player knocked out");
 			//reduce number of life remaining to the player
 		}
 	}
 
 	public int getHealth() {
-		return health;
+		return hitPoints;
 	}
 	
 }
