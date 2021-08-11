7f0fa8309d5ba566797f81818de307b035874de1
benNjogu <kamandemachafi.gmail.com>
2021-08-11 10:32:14 +0300

    again

--- PolyMorphism/src/com/keytech/Main.java
+++ PolyMorphism/src/com/keytech/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("doing Polymorphism again");
+		System.out.println("doing Polymorphism again and again");
 
 	}
 
