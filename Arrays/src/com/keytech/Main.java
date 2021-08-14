5bbae162e9b47ead59fef429ae8f6d64b6bc6cd9
benNjogu <kamandemachafi.gmail.com>
2021-08-14 17:41:32 +0300

    declaring an array and multiplication table

--- Arrays/src/com/keytech/Main.java
+++ Arrays/src/com/keytech/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int[] myIntArray = new int[10];
+		myIntArray[4] = 48;
+		int number = myIntArray[4];
+		
+		//another way to do this 
+		int[] myOtherIntArray = {1,2,3,4,5,6,7,8,9,10};
+		int num = 459874;
+		for(int i=0; i<=myOtherIntArray.length; i++) {
+
+			System.out.println(num +"*"+ i+" = "+num*i);
+			
+		}
+		
 	}
 	
 }
