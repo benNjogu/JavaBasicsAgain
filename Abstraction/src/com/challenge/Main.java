57bd62ddd4c4b435085a9fe4e39beb9b22620dc2
benNjogu <kamandemachafi.gmail.com>
2021-09-11 11:54:58 +0300

    removing items

--- Abstraction/src/com/challenge/Main.java
+++ Abstraction/src/com/challenge/Main.java
@@ -5,8 +5,33 @@ public class Main {
 	public static void main(String[] args) {
 		
 		MyLinkedList list = new MyLinkedList(null);
+		//list.traverse(list.getRoot());
+		
+		//String stringData = "Murang'a Kenol Thika Nairobi Uthiru Limuru Kijabe Naivasha Gilgil Lanet Nakuru Murang'a";
+		String stringData = "5 7 3 8 2 1 0 4 6";
+		String[] data = stringData.split(" ");
+		for (String s : data) {
+			//create a new item
+			list.addItem(new Node(s));
+		}
+		list.traverse(list.getRoot());
+		list.removeItem(new Node("3"));
+		
+		list.traverse(list.getRoot());
+		list.removeItem(new Node("8"));
+		list.removeItem(new Node("7"));
 		list.traverse(list.getRoot());
 		
+		list.removeItem(new Node("0"));
+		list.removeItem(new Node("4"));
+		list.removeItem(new Node("6"));
+		list.removeItem(new Node("2"));
+		list.traverse(list.getRoot());
+
+		list.removeItem(new Node("1"));
+		list.traverse(list.getRoot());
+		list.removeItem(list.getRoot());
+		list.traverse(list.getRoot());
 	}
 	
 }
--- Abstraction/src/com/challenge/MyLinkedList.java
+++ Abstraction/src/com/challenge/MyLinkedList.java
@@ -1,6 +1,6 @@
 package com.challenge;
 
-public class MyLinkedList implements NodeList{
+public class MyLinkedList implements NodeList {
 
 	private ListItem root = null;
 
@@ -16,63 +16,92 @@ public class MyLinkedList implements NodeList{
 
 	@Override
 	public boolean addItem(ListItem newItem) {
-		if(this.root == null) {
-			//list is empty, so this item becomes the head
+		if (this.root == null) {
+			// list is empty, so this item becomes the head
 			this.root = newItem;
 			return true;
 		}
 		ListItem currentItem = this.root;
 		while (currentItem != null) {
 			int comparison = (currentItem.compareTo(newItem));
-			if(comparison<0) {
-				//new item is greater, move right if possible
-				if(currentItem.next() != null) {
+			if (comparison < 0) {
+				// new item is greater, move right if possible
+				if (currentItem.next() != null) {
 					currentItem = currentItem.next();
-				}else {
-					//there is no next insert at the end of the list
+				} else {
+					// there is no next insert at the end of the list
 					currentItem.setNext(newItem).setPrevious(currentItem);
 					return true;
 				}
-			}else if (comparison>0) {
-				//new item is less, insert b4
+			} else if (comparison > 0) {
+				// new item is less, insert b4
 				if (currentItem.previous() != null) {
 					currentItem.previous().setNext(newItem).setPrevious(currentItem.previous());
 					newItem.setNext(currentItem).setPrevious(newItem);
-				}else {
-					//the node without the previous is the root
+				} else {
+					// the node without the previous is the root
 					newItem.setNext(this.root).setPrevious(newItem);
 					this.root = newItem;
 				}
 
 				return true;
-			}else {
-				//they are equal
-				System.out.println(newItem.getValue()+" is already present, not added.");
+			} else {
+				// they are equal
+				System.out.println(newItem.getValue() + " is already present, not added.");
 
 				return false;
 			}
 		}
-		
+
 		return false;
 	}
 
 	@Override
 	public boolean removeItem(ListItem item) {
+		if (item != null) {
+			System.out.println("Deleting item " + item.getValue());
+
+		}
+
+		ListItem currentItem = this.root;
+		while (currentItem != null) {
+			int comparison = currentItem.compareTo(item);
+			if (comparison == 0) {
+				//found the item to delete
+				if(currentItem == this.root) {
+					this.root = currentItem.next();
+				}else {
+					currentItem.previous().setNext(currentItem.next());
+					if(currentItem.next()!=null) {
+						currentItem.next().setPrevious(currentItem.previous());
+					}
+				}
+				return true;
+			}else if (comparison<0) {
+				currentItem = currentItem.next();
+			}else {
+				//comparison > 0
+				//means item is not in the list
+				return false;
+			}
+		}
+		
+		//reached the end of the list
+		//No item to delete
 		return false;
 	}
 
 	@Override
 	public void traverse(ListItem root) {
-		if(root == null) {
-			System.out.println("The root is empty");
-		}else {
-			while(root!=null) {
+		if (root == null) {
+			System.out.println("The list is empty");
+		} else {
+			while (root != null) {
 				System.out.println(root.getValue());
 				root = root.next();
 			}
 		}
-		
+
 	}
-	
-	
+
 }
