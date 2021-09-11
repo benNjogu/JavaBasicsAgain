59f3f6a4be905735ddd070969326e1712eabef01
benNjogu <kamandemachafi.gmail.com>
2021-09-11 12:43:33 +0300

    search tree

---
+++ Abstraction/src/com/challenge/SearchTree.java
@@ -0,0 +1,39 @@
+package com.challenge;
+
+public class SearchTree implements NodeList{
+
+	private ListItem root;
+	
+	public SearchTree(ListItem root) {
+		super();
+		this.root = root;
+	}
+
+	@Override
+	public ListItem getRoot() {
+		return this.root;
+	}
+
+	@Override
+	public boolean addItem(ListItem item) {
+		return false;
+	}
+
+	@Override
+	public boolean removeItem(ListItem item) {
+		return false;
+	}
+
+	@Override
+	public void traverse(ListItem root) {
+
+		//recursive method
+		if(root != null) {
+			traverse(root.previous());
+			System.out.println(root.getValue());
+			traverse(root.next());
+		}
+		
+	}
+
+}
