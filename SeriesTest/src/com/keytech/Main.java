1dfbb7a1ffaecd165dcf1d486741b16927ce8bd1
benNjogu <kamandemachafi.gmail.com>
2021-09-14 21:49:25 +0300

    using my jar file package

---
+++ SeriesTest/src/com/keytech/Main.java
@@ -0,0 +1,21 @@
+package com.keytech;
+
+import com.keytech.mylibrary.Series;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) {
+			System.out.print(Series.nSum(i)+" ");
+		}
+		System.out.println();
+		for (int i = 0; i <= 10; i++) {
+			System.out.print(Series.factorial(i)+" ");
+		}
+		System.out.println();
+		for (int i = 0; i <= 10; i++) {
+			System.out.print(Series.fibonacci(i)+" ");
+		}
+	}
+	
+}
