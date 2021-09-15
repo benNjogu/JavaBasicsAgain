75981a0fbf140196b3bd3f95267e90a22d6b219a
benNjogu <kamandemachafi.gmail.com>
2021-09-15 16:25:05 +0300

    tims way 2

--- Scope/src/com/timsway/Main.java
+++ Scope/src/com/timsway/Main.java
@@ -3,13 +3,9 @@ package com.timsway;
 import java.util.Scanner;
 
 public class Main {
-
-	private static Scanner scanner = new Scanner(System.in);
-	
 	public static void main(String[] args) {
 		
-		System.out.println("Please enter a number ");
-		X x = new X(scanner.nextInt());
+		X x = new X(new Scanner(System.in));
 		x.x();
 		
 	}
--- Scope/src/com/timsway/X.java
+++ Scope/src/com/timsway/X.java
@@ -1,11 +1,14 @@
 package com.timsway;
 
+import java.util.Scanner;
+
 public class X {
 
 	private int x;
 
-	public X(int x) {
-		this.x = x;
+	public X(Scanner x) {
+		System.out.println("Enter a number: ");
+		this.x = x.nextInt();
 	}
 	
 	public void x() {
