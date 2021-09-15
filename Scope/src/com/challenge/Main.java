523dfac5d5d87682798dbda3d72abb927719c4de
benNjogu <kamandemachafi.gmail.com>
2021-09-15 15:46:16 +0300

    my way

---
+++ Scope/src/com/challenge/Main.java
@@ -0,0 +1,19 @@
+package com.challenge;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner scanner = new Scanner(System.in);
+	private static int x;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the number you want to know table ");
+		
+		X x = new X();
+		x.x(scanner.nextInt());
+		
+	}
+	
+}
---
+++ Scope/src/com/challenge/X.java
@@ -0,0 +1,13 @@
+package com.challenge;
+
+public class X {
+
+	private int x;
+	
+	public void x(int x){
+		for (this.x = 1; this.x <= 12; this.x++) {
+			System.out.println(x+" * "+this.x+" = "+(this.x*x));
+		}
+	}
+	
+}
