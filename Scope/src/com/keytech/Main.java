12f6d39648133316398b899cc4678e2b985c2913
benNjogu <kamandemachafi.gmail.com>
2021-09-15 13:09:17 +0300

    scope part 2

--- Scope/src/com/keytech/Main.java
+++ Scope/src/com/keytech/Main.java
@@ -7,14 +7,15 @@ public class Main {
 		String privateVar = "this is private for main()";
 		
 		ScopeCheck scopeInstance = new ScopeCheck();
-		System.out.println("scopeInstance privateVar is "+scopeInstance.getPrivateVar());
+		
+		System.out.println("scopeInstance varThree is "+scopeInstance.getVarOne());
 		System.out.println(privateVar);
 		
 		scopeInstance.timesTwo();
 		
 		ScopeCheck.InnerClass iClass = scopeInstance.new InnerClass();
 		iClass.timesTwo();
-		
+		scopeInstance.useInner();
 	}
 	
 }
