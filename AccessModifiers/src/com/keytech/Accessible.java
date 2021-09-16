eb584d9e4f63b99d1c5e9c9012192646a51319f5
benNjogu <kamandemachafi.gmail.com>
2021-09-16 12:01:20 +0300

    Access modifiers part 1

---
+++ AccessModifiers/src/com/keytech/Accessible.java
@@ -0,0 +1,5 @@
+package com.keytech;
+
+public interface Accessible {
+
+}
---
+++ AccessModifiers/src/com/keytech/Main.java
@@ -0,0 +1,19 @@
+package com.keytech;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Account kimsAccount = new Account("Kim");
+		kimsAccount.deposit(1000);
+		kimsAccount.withdraw(500);
+		kimsAccount.withdraw(200);
+		kimsAccount.withdraw(-200);
+		kimsAccount.deposit(-20);
+		kimsAccount.calculateBalance();
+		
+		System.out.println("Balance on account is "+kimsAccount.getBalance());
+		
+	}
+	
+}
