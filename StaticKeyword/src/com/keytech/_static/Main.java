57784fd003963d3707ac67cb486d63ba161b963c
benNjogu <kamandemachafi.gmail.com>
2021-09-16 13:03:14 +0300

    static keyword part 1

---
+++ StaticKeyword/src/com/keytech/_static/Main.java
@@ -0,0 +1,18 @@
+package com.keytech._static;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		StaticTest firstInstance = new StaticTest("1st instance");
+		System.out.println(firstInstance.getName()+" is instance number "+StaticTest.getNumInstances());
+		
+		StaticTest secondInstance = new StaticTest("2nd instance");
+		System.out.println(secondInstance.getName()+" is instance number "+StaticTest.getNumInstances());
+		
+		StaticTest thirdInstance = new StaticTest("3rd instance");
+		System.out.println(thirdInstance.getName()+" is instance number "+StaticTest.getNumInstances());
+		
+	}
+	
+}
