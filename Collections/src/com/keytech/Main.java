736f8696b664313a3f362c76f3b922f302a386ee
benNjogu <kamandemachafi.gmail.com>
2021-09-17 10:16:10 +0300

    collections overview

---
+++ Collections/src/com/keytech/Main.java
@@ -0,0 +1,17 @@
+package com.keytech;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Theatre theatre = new Theatre("KICC", 8, 12);
+		theatre.getSeats();
+		
+		theatre.reserveSeat("H11");
+		theatre.reserveSeat("H11");
+		theatre.reserveSeat("H11");
+		theatre.reserveSeat("H11");
+		
+	}
+	
+}
