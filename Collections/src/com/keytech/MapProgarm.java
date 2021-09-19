ac556c5815e70e6ac4d2b14746ca8138f99a35d0
benNjogu <kamandemachafi.gmail.com>
2021-09-19 12:14:44 +0300

    maps 2

--- Collections/src/com/keytech/MapProgarm.java
+++ Collections/src/com/keytech/MapProgarm.java
@@ -6,16 +6,29 @@ import java.util.Map;
 public class MapProgarm {
 
 	public static void main(String[] args) {
-		
-		Map<String, String> languages = new HashMap<>();	
+
+		Map<String, String> languages = new HashMap<>();
 		languages.put("Java", "High level, Object Oriented, Platform independent language");
-		languages.put("Python", "An interpreted, Object oriented, High level programing language with dynamic semantics");
+		languages.put("Python",
+				"An interpreted, Object oriented, High level programing language with dynamic semantics");
 		languages.put("Algol", "An algorithmic language");
 		languages.put("Basic", "Beginners all purpose symbolic instruction code");
 		languages.put("Lisp", "There in lies madness");
-		
+
+		if (languages.containsKey("Java")) {
+			System.out.println("Java already exists in the map");
+		} else {
+			languages.put("Java", "what i want to preach");
+		}
+
 		System.out.println(languages.get("Java"));
-		
+
+		System.out.println("============================================================================");
+
+		for (String key : languages.keySet()) {
+			System.out.println(key+" : "+languages.get(key));
+		}
+
 	}
-	
+
 }
