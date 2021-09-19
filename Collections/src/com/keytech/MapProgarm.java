23738547f58ac754bb0558bbfbd10cc9c1a13fbc
benNjogu <kamandemachafi.gmail.com>
2021-09-19 11:53:38 +0300

    Maps

---
+++ Collections/src/com/keytech/MapProgarm.java
@@ -0,0 +1,21 @@
+package com.keytech;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgarm {
+
+	public static void main(String[] args) {
+		
+		Map<String, String> languages = new HashMap<>();	
+		languages.put("Java", "High level, Object Oriented, Platform independent language");
+		languages.put("Python", "An interpreted, Object oriented, High level programing language with dynamic semantics");
+		languages.put("Algol", "An algorithmic language");
+		languages.put("Basic", "Beginners all purpose symbolic instruction code");
+		languages.put("Lisp", "There in lies madness");
+		
+		System.out.println(languages.get("Java"));
+		
+	}
+	
+}
