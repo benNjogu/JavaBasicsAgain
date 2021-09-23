3b0fa25c070c76bd5a2bb795823c3d5f9ccd8421
benNjogu <kamandemachafi.gmail.com>
2021-09-23 18:26:08 +0300

    with redundant Q

--- Adventure/src/com/keytech/Main.java
+++ Adventure/src/com/keytech/Main.java
@@ -18,6 +18,29 @@ public class Main {
 		locations.put(4, new Location(4, "You are on your way to school"));
 		locations.put(5, new Location(5, "You are conversing with friends"));
 		locations.put(6, new Location(6, "You are on your way to pee"));
+		
+		locations.get(1).addExit("W", 2);
+		locations.get(1).addExit("E", 3);
+		locations.get(1).addExit("S", 4);
+		locations.get(1).addExit("N", 5);
+		locations.get(1).addExit("Q", 0);
+		
+		locations.get(2).addExit("N", 5);
+		locations.get(2).addExit("Q", 0);
+		
+		locations.get(3).addExit("W", 1);
+		locations.get(3).addExit("Q", 0);
+		
+
+
+		locations.get(4).addExit("N", 1);
+		locations.get(4).addExit("W", 2);
+		locations.get(4).addExit("Q", 0);
+		
+		locations.get(5).addExit("S", 1);
+		locations.get(5).addExit("W", 2);
+		locations.get(5).addExit("Q", 0);
+		
 
 		int loc = 1;
 		while (true) {
@@ -26,11 +49,23 @@ public class Main {
 				break;
 			}
 
-			loc = scanner.nextInt();
-			if (!locations.containsKey(loc)) {
-				System.out.println("You cannot go in that direction");
-				break;
+			Map<String, Integer> exits = locations.get(loc).getExits();
+			System.out.print("Available exits are ");
+			for (String exit : exits.keySet()) {
+				System.out.print(exit+", ");
 			}
+			System.out.println();
+			
+			String direction = scanner.nextLine().toUpperCase();
+			
+			if (exits.containsKey(direction)) {
+
+				loc = exits.get(direction);
+			}else {
+
+				System.out.println("You cannot go in that direction");
+			}
+			
 		}
 
 	}
