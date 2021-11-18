2ab6d9df83e2a11f10aa7ce0cd019d768ba89ff8
benNjogu <kamandemachafi.gmail.com>
2021-11-18 21:03:46 +0300

    Using a for each loop

--- Lambda Expressions/src/com/keytech/LambdaExpression.java
+++ Lambda Expressions/src/com/keytech/LambdaExpression.java
@@ -8,8 +8,8 @@ public class LambdaExpression {
 	public static void main(String[] args) {
 		List<Integer> values = Arrays.asList(4,5,6,7,8);
 		
-		for(int i=0; i<values.size(); i++) {
-			System.out.println(values.get(i));
+		for(int i : values) {
+			System.out.println(i);
 		}
 	}
 
