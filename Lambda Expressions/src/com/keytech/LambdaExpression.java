20105f1a8c3adafab11624dc0cac7b4a4dce0d7a
benNjogu <kamandemachafi.gmail.com>
2021-11-18 21:07:14 +0300

    Using lambda

--- Lambda Expressions/src/com/keytech/LambdaExpression.java
+++ Lambda Expressions/src/com/keytech/LambdaExpression.java
@@ -8,9 +8,7 @@ public class LambdaExpression {
 	public static void main(String[] args) {
 		List<Integer> values = Arrays.asList(4,5,6,7,8);
 		
-		for(int i : values) {
-			System.out.println(i);
-		}
+		values.forEach(i -> System.out.println(i));//lambda
 	}
 
 }
