c41c39625a74df99e2db424f482724e5b0d7b78e
benNjogu <kamandemachafi.gmail.com>
2021-11-19 10:48:20 +0300

    Using an anonymous inner class

--- Lambda Expressions/src/com/keytech/LamdaDemo.java
+++ Lambda Expressions/src/com/keytech/LamdaDemo.java
@@ -4,21 +4,26 @@ interface A{
 	void show();
 }
 
-class B implements A{
-
-	@Override
-	public void show() {
-		System.out.println("Hello lambda!");
-	}
-	
-}
+//class B implements A{
+//
+//	@Override
+//	public void show() {
+//		System.out.println("Hello lambda!");
+//	}
+//	
+//}
 
 public class LamdaDemo {
 
 	public static void main(String[] args) {
 		A a;
-		a = new B();
+		a = new A() { //using an anonymous inner class.
+			public void show() {
+				System.out.println("Hello");
+			}
+		};
 		a.show();
+		
 	}
 	
 }
