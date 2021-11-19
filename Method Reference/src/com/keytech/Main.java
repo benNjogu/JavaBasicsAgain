76c24239daa2308209bbf1dc04cb8462f369b1ee
benNjogu <kamandemachafi.gmail.com>
2021-11-19 13:11:43 +0300

    Add brackets to the expression

--- Method Reference/src/com/keytech/Main.java
+++ Method Reference/src/com/keytech/Main.java
@@ -35,7 +35,7 @@ public class Main {
 		String str = "Faith Nyoko";
 		
 		MyPrinter mPrinter = new MyPrinter();
-		mPrinter.print(str, StringParser::convert);
+		mPrinter.print(str, (StringParser::convert));
 		
 	}
 	
