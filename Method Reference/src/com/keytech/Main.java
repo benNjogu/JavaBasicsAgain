24c46bcbaa006d3a365078bf61bf96ce7b51f434
benNjogu <kamandemachafi.gmail.com>
2021-11-19 12:55:35 +0300

    implementing using inner class

--- Method Reference/src/com/keytech/Main.java
+++ Method Reference/src/com/keytech/Main.java
@@ -1,16 +1,48 @@
 package com.keytech;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
+interface Parser {
+	
+	String parse(String str);
+	
+}
+
+class StringParser{
+	
+	public static String convert(String s) {
+		if (s.length()<=3) {
+			s = s.toUpperCase();
+		}else {
+			s = s.toLowerCase();
+		}
+		return s;
+	}
+	
+}
+
+class MyPrinter{
+	
+	public void print(String str, Parser p) {
+		str = p.parse(str);
+		System.out.println(str);
+	}
+	
+}
 
 public class Main {
 
 	public static void main(String[] args) {
+
+		String str = "Faith Nyoko";
 		
-		List<String> names = Arrays.asList("Ben","Njau", "Kamande","Muiruri", "Wanjiru","Wangui");
+		MyPrinter mPrinter = new MyPrinter();
+		mPrinter.print(str, new Parser() {
+			
+			public String parse(String s) {
+				
+				return StringParser.convert(s);
+			}
+		});
 		
-		names.forEach(System.out::println);
 	}
 	
 }
