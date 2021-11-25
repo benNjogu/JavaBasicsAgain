fd1f8a476d27598ed784578e845f74005aa37f8c
benNjogu <kamandemachafi.gmail.com>
2021-11-25 11:32:22 +0300

    Printing normally

--- Method Reference Again/src/Main.java
+++ Method Reference Again/src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		String[] nameArray = {"ben","basili","daagi","nyoko","chamigeigh","Kamjoro","cha pa"};
+		System.out.println(Arrays.asList(nameArray));
+		
+		
 	}
 
 }
