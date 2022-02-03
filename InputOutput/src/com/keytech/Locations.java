712d96cbd0dcb393288efab04fc4a577db06bf66
benNjogu <kamandemachafi.gmail.com>
2022-02-03 05:11:36 +0300

    try and finally blocks

--- InputOutput/src/com/keytech/Locations.java
+++ InputOutput/src/com/keytech/Locations.java
@@ -7,66 +7,60 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class Locations implements Map<Integer, Location>{
+public class Locations implements Map<Integer, Location> {
 
 	private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		FileWriter locFile = null;
 		try {
 			locFile = new FileWriter("locations.txt");
-			for(Location location : locations.values()) {
-				locFile.write(location.getLocationalId()+","+location.getDescription()+"\n");
+			for (Location location : locations.values()) {
+				locFile.write(location.getLocationalId() + "," + location.getDescription() + "\n");
 			}
 
-		} catch (IOException e) {
-			System.out.println("In catch block");
-			e.printStackTrace();
-		}finally {
+		} finally {
 			System.out.println("In finally block");
-			try {
-				if (locFile != null) {
-					System.out.println("Attempting to close locFile");
-					locFile.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-	}
-	
-	static {
-		Map<String, Integer> tempExit = new HashMap<String,Integer>();
-		locations.put(0, new Location(0, "You are sitting in front of a computer learning java", tempExit));
-		
 
-		tempExit = new HashMap<String,Integer>();
+			if (locFile != null) {
+				System.out.println("Attempting to close locFile");
+				locFile.close();
+			}
+
+		}
+
+	}
+
+	static {
+		Map<String, Integer> tempExit = new HashMap<String, Integer>();
+		locations.put(0, new Location(0, "You are sitting in front of a computer learning java", tempExit));
+
+		tempExit = new HashMap<String, Integer>();
 		tempExit.put("W", 2);
 		tempExit.put("E", 3);
 		tempExit.put("S", 4);
 		tempExit.put("N", 5);
 		locations.put(1, new Location(1, "You are headed for a ride", tempExit));
 
-		tempExit = new HashMap<String,Integer>();
+		tempExit = new HashMap<String, Integer>();
 		tempExit.put("N", 5);
 		locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
 
-		tempExit = new HashMap<String,Integer>();
+		tempExit = new HashMap<String, Integer>();
 		tempExit.put("W", 1);
 		locations.put(3, new Location(3, "You are moving sometime ", tempExit));
 
-		tempExit = new HashMap<String,Integer>();
+		tempExit = new HashMap<String, Integer>();
 		tempExit.put("N", 1);
 		tempExit.put("W", 2);
 		locations.put(4, new Location(4, "You are on your way to school", tempExit));
 
-		tempExit = new HashMap<String,Integer>();
+		tempExit = new HashMap<String, Integer>();
 		tempExit.put("S", 1);
 		tempExit.put("W", 2);
 		locations.put(5, new Location(5, "You are conversing with friends", null));
 	}
-	
+
 	@Override
 	public void clear() {
 		locations.clear();
@@ -110,7 +104,7 @@ public class Locations implements Map<Integer, Location>{
 	@Override
 	public void putAll(Map<? extends Integer, ? extends Location> arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -127,6 +121,5 @@ public class Locations implements Map<Integer, Location>{
 	public Collection<Location> values() {
 		return locations.values();
 	}
-	
-	
+
 }
