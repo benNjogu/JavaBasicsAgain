af80594e1368f26e0b1d917aec4c99e22a563b25
benNjogu <kamandemachafi.gmail.com>
2022-02-03 07:40:12 +0300

    BufferedReader

--- InputOutput/src/com/keytech/Locations.java
+++ InputOutput/src/com/keytech/Locations.java
@@ -1,5 +1,6 @@
 package com.keytech;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -67,34 +68,62 @@ public class Locations implements Map<Integer, Location> {
 			}
 		}
 
-		// Map<String, Integer> tempExit = new HashMap<String, Integer>();
-		// locations.put(0, new Location(0, "You are sitting in front of a computer
-		// learning java", tempExit));
-		//
-		// tempExit = new HashMap<String, Integer>();
-		// tempExit.put("W", 2);
-		// tempExit.put("E", 3);
-		// tempExit.put("S", 4);
-		// tempExit.put("N", 5);
-		// locations.put(1, new Location(1, "You are headed for a ride", tempExit));
-		//
-		// tempExit = new HashMap<String, Integer>();
-		// tempExit.put("N", 5);
-		// locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
-		//
-		// tempExit = new HashMap<String, Integer>();
-		// tempExit.put("W", 1);
-		// locations.put(3, new Location(3, "You are moving sometime ", tempExit));
-		//
-		// tempExit = new HashMap<String, Integer>();
-		// tempExit.put("N", 1);
-		// tempExit.put("W", 2);
-		// locations.put(4, new Location(4, "You are on your way to school", tempExit));
-		//
-		// tempExit = new HashMap<String, Integer>();
-		// tempExit.put("S", 1);
-		// tempExit.put("W", 2);
-		// locations.put(5, new Location(5, "You are conversing with friends", null));
+		// Now read the exits
+		try {
+			scanner = new Scanner(new BufferedReader(new FileReader("directions.txt")));
+			scanner.useDelimiter(",");
+			while (scanner.hasNextLine()) {
+//				int loc = scanner.nextInt();
+//				scanner.skip(scanner.delimiter());
+//				String direction = scanner.next();
+//				scanner.skip(scanner.delimiter());
+//				String dest = scanner.nextLine();
+//				int destination = Integer.parseInt(dest);
+				
+				String input = scanner.nextLine();
+				String[] data = input.split(",");
+				int loc = Integer.parseInt(data[0]);
+				String direction = data[1];
+				int destination = Integer.parseInt(data[2]);
+				System.out.println(loc + ": " + direction + ": " + destination);
+				Location location = locations.get(loc);
+				location.addExit(direction, destination);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+		}
+
+//		 Map<String, Integer> tempExit = new HashMap<String, Integer>();
+//		 locations.put(0, new Location(0, "You are sitting in front of a computer learning java", tempExit));
+//		
+//		 tempExit = new HashMap<String, Integer>();
+//		 tempExit.put("W", 2);
+//		 tempExit.put("E", 3);
+//		 tempExit.put("S", 4);
+//		 tempExit.put("N", 5);
+//		 locations.put(1, new Location(1, "You are headed for a ride", tempExit));
+//		
+//		 tempExit = new HashMap<String, Integer>();
+//		 tempExit.put("N", 5);
+//		 locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
+//		
+//		 tempExit = new HashMap<String, Integer>();
+//		 tempExit.put("W", 1);
+//		 locations.put(3, new Location(3, "You are moving sometime ", tempExit));
+//		
+//		 tempExit = new HashMap<String, Integer>();
+//		 tempExit.put("N", 1);
+//		 tempExit.put("W", 2);
+//		 locations.put(4, new Location(4, "You are on your way to school", tempExit));
+//		
+//		 tempExit = new HashMap<String, Integer>();
+//		 tempExit.put("S", 1);
+//		 tempExit.put("W", 2);
+//		 locations.put(5, new Location(5, "You are conversing with friends", null));
 	}
 
 	@Override
