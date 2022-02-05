2fa15e58290d1f2d9c3d84bea985a5d23dd4bee5
benNjogu <kamandemachafi.gmail.com>
2022-02-05 16:30:49 +0300

    Getting rid of unnecessary comments.

--- InputOutput/src/com/keytech/Locations.java
+++ InputOutput/src/com/keytech/Locations.java
@@ -11,6 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Collection;
@@ -25,26 +26,6 @@ public class Locations implements Map<Integer, Location> {
 	private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
 	public static void main(String[] args) throws IOException {
-		// try (DataOutputStream locFile = new DataOutputStream(
-		// new BufferedOutputStream(new FileOutputStream("locations.dat")))) {
-		// for (Location location : locations.values()) {
-		// locFile.writeInt(location.getLocationId());
-		// locFile.writeUTF(location.getDescription());
-		// System.out.println("Writing location " + location.getLocationId() + ":" +
-		// location.getDescription());
-		// System.out.println("Writing " + (location.getExits().size() - 1) + "
-		// exits.");
-		// locFile.writeInt(location.getExits().size() - 1);
-		// for (String direction : location.getExits().keySet()) {
-		// if (!direction.equalsIgnoreCase("Q")) {
-		// System.out.println("\t\t" + direction + "," +
-		// location.getExits().get(direction));
-		// locFile.writeUTF(direction);
-		// locFile.writeInt(location.getExits().get(direction));
-		// }
-		// }
-		// }
-		// }
 
 		try (ObjectOutputStream locFile = new ObjectOutputStream(
 				new BufferedOutputStream(new FileOutputStream("locations.dat")))) {
@@ -72,32 +53,13 @@ public class Locations implements Map<Integer, Location> {
 				}
 
 			}
+		} catch (InvalidClassException e) {
+			System.out.println("InvalidClassException " + e.getMessage());
 		} catch (IOException e) {
-			System.out.println("IO Exception "+e.getMessage());
-		}catch (ClassNotFoundException e) {
-			System.out.println("ClassNotFoundException "+e.getMessage());
+			System.out.println("IO Exception " + e.getMessage());
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException " + e.getMessage());
 		}
-		// while (!eof) {
-		// try {
-		// Map<String, Integer> exits = new LinkedHashMap<>();
-		// int locID = locFile.readInt();
-		// String description = locFile.readUTF();
-		// int numExits = locFile.readInt();
-		// System.out.println("Read location " + locID + " : " + description);
-		// System.out.println("Found " + numExits + " exits");
-		// for (int i = 0; i < numExits; i++) {
-		// String direction = locFile.readUTF();
-		// int destination = locFile.readInt();
-		// exits.put(direction, destination);
-		// System.out.println("\t\t" + direction + "," + destination);
-		// }
-		//
-		// locations.put(locID, new Location(locID, description, exits));
-		// } catch (EOFException e) {
-		// eof = true;
-		// }
-		//
-		// }
 
 	}
 
