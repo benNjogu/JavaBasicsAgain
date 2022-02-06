b4a16e3353bcaa57460da0efbdd0f3b1c5514d06
benNjogu <kamandemachafi.gmail.com>
2022-02-06 19:00:33 +0300

    Receive response

--- UDPClient/src/com/keytech/client/Main.java
+++ UDPClient/src/com/keytech/client/Main.java
@@ -8,12 +8,14 @@ import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		try {
-			InetAddress address = InetAddress.getLocalHost();//getByName()
+			InetAddress address = InetAddress.getLocalHost();//getBN
 			DatagramSocket socket = new DatagramSocket();
 			
 			Scanner scanner = new Scanner(System.in);
@@ -26,6 +28,12 @@ public class Main {
 				byte[] buffer = echoString.getBytes();
 				DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 5000);
 				socket.send(packet);
+				
+				//response not needed in real world application
+				byte[] buffer2 = new byte[50];
+				packet = new DatagramPacket(buffer2, buffer2.length);
+				socket.receive(packet);
+				System.out.println("Text received is: " + new String(buffer2,0,packet.getLength()));
 				
 			} while (!echoString.equals("exit"));
 			
