6f343524c1c07f63dc93f4672864a94efe39c63e
benNjogu <kamandemachafi.gmail.com>
2022-02-06 18:44:50 +0300

    Sending response to client.

--- UDPServer/src/com/keytech/server/Main.java
+++ UDPServer/src/com/keytech/server/Main.java
@@ -3,6 +3,7 @@ package com.keytech.server;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 public class Main {
@@ -16,7 +17,14 @@ public class Main {
 				byte[] buffer = new byte[50];
 				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 				socket.receive(packet);
-				System.out.println("Text received is: " + new String(buffer));
+				System.out.println("Text received is: " + new String(buffer,0,packet.getLength()));
+				
+				String returnString = "echo: " + new String(buffer,0,packet.getLength());
+				byte[] buffer2 = returnString.getBytes();	
+				InetAddress address = packet.getAddress();
+				int port = packet.getPort();
+				packet = new DatagramPacket(buffer2, buffer2.length, address, port);
+				socket.send(packet);
 			}
 			
 		} catch (SocketException e) {
