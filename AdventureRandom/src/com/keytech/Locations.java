760cb3df68fdc11b9920fe7fdcfe57614bc638ea
benNjogu <kamandemachafi.gmail.com>
2022-02-06 11:40:44 +0300

    Reading from Random Access Files

--- AdventureRandom/src/com/keytech/Locations.java
+++ AdventureRandom/src/com/keytech/Locations.java
@@ -17,6 +17,7 @@ public class Locations implements Map<Integer, Location> {
 
 	private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
 	private static Map<Integer, IndexRecord> index = new LinkedHashMap<>();
+	private static RandomAccessFile ra;
 
 	public static void main(String[] args) throws IOException {
 
@@ -72,31 +73,47 @@ public class Locations implements Map<Integer, Location> {
 	}
 
 	static {
-		try (ObjectInputStream locFile = new ObjectInputStream(
-				new BufferedInputStream(new FileInputStream("locations.dat")))) {
-
-			boolean eof = false;
-			while (!eof) {
-				try {
-					Location location = (Location) locFile.readObject();
-					System.out.println("Read location " + location.getLocationId() + " : " + location.getDescription());
-					System.out.println("Found " + location.getExits().size() + " exits");
-					locations.put(location.getLocationId(), location);
-				} catch (EOFException e) {
-					eof = true;
-				}
-
+		try {
+			ra = new RandomAccessFile("locations_rand.dat", "rwd");
+			int numLocations = ra.readInt();
+			long locationStartPoint = ra.readInt();
+			while(ra.getFilePointer() < locationStartPoint) {
+				int locationId = ra.readInt();
+				int locationStart = ra.readInt();
+				int locationLength = ra.readInt();
+				
+				IndexRecord record = new IndexRecord(locationStart, locationLength);
+				index.put(locationId, record);
 			}
-		} catch (InvalidClassException e) {
-			System.out.println("InvalidClassException " + e.getMessage());
+			
 		} catch (IOException e) {
-			System.out.println("IO Exception " + e.getMessage());
-		} catch (ClassNotFoundException e) {
-			System.out.println("ClassNotFoundException " + e.getMessage());
+			System.out.println("IOException in static initializer: " + e.getMessage());
 		}
-
 	}
 
+	public Location getLocation(int locationId) throws IOException {
+		IndexRecord record = index.get(locationId);
+		ra.seek(record.getStartByte());
+		int id = ra.readInt();
+		String description = ra.readUTF();
+		String exits = ra.readUTF();
+		String[] exitPart = exits.split(",");
+		
+		Location location = new Location(locationId, description, null);
+		
+		if (locationId != 0) {
+			for (int i = 0; i < exitPart.length; i++) {
+				System.out.println("exitPart = " + exitPart[i]);
+				System.out.println("exitPart[+1] " + exitPart[i + 1]);
+				String direction = exitPart[i];
+				int destination = Integer.parseInt(exitPart[++i]);
+				location.addExit(direction, destination);
+			}
+		}
+		
+		return location;
+	}
+	
 	@Override
 	public void clear() {
 		locations.clear();
@@ -156,6 +173,10 @@ public class Locations implements Map<Integer, Location> {
 	@Override
 	public Collection<Location> values() {
 		return locations.values();
+	}
+	
+	public void close() throws IOException {
+		ra.close();
 	}
 
 }
--- AdventureRandom/src/com/keytech/Main.java
+++ AdventureRandom/src/com/keytech/Main.java
@@ -1,5 +1,6 @@
 package com.keytech;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -9,7 +10,7 @@ public class Main {
 	private static Scanner scanner = new Scanner(System.in);
 	private static Locations locations = new Locations();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Map<String, String> vocabulary = new HashMap<String, String>();
 		vocabulary.put("QUIT", "Q");
@@ -18,14 +19,16 @@ public class Main {
 		vocabulary.put("WEST", "W");
 		vocabulary.put("EAST", "E");
 
-		int loc = 1;
+		//int loc = 64;
+		Location currentLocation = locations.getLocation(64); 
+				
 		while (true) {
-			System.out.println(locations.get(loc).getDescription());
-			if (loc == 0) {
+			System.out.println(currentLocation.getDescription());
+			if (currentLocation.getLocationId() == 0) {
 				break;
 			}
 
-			Map<String, Integer> exits = locations.get(loc).getExits();
+			Map<String, Integer> exits = currentLocation.getExits();
 			System.out.print("Available exits are ");
 			for (String exit : exits.keySet()) {
 				System.out.print(exit + ", ");
@@ -45,7 +48,7 @@ public class Main {
 
 			if (exits.containsKey(direction)) {
 
-				loc = exits.get(direction);
+				currentLocation = locations.getLocation(currentLocation.getExits().get(direction));
 			} else {
 
 				System.out.println("You cannot go in that direction");
@@ -53,6 +56,7 @@ public class Main {
 
 		}
 
+		locations.close();
 	}
 
 }
