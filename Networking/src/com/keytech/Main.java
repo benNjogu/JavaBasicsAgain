293294aaf0b1f0211a5e0e931c844364cae119e4
benNjogu <kamandemachafi.gmail.com>
2022-02-07 09:50:30 +0300

    Malformed url exception

--- Networking/src/com/keytech/Main.java
+++ Networking/src/com/keytech/Main.java
@@ -1,39 +1,33 @@
 package com.keytech;
 
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
 			URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phone?os=android#samsung");
-
-			System.out.println("Scheme = " + uri.getScheme());
-			System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-			System.out.println("Authority = " + uri.getAuthority());
-			System.out.println("User info = " + uri.getUserInfo());
-			System.out.println("Host = " + uri.getHost());
-			System.out.println("Port = " + uri.getPort());
-			System.out.println("Path = " + uri.getPath());
-			System.out.println("Query = " + uri.getQuery());
-			System.out.println("Fragment = " + uri.getFragment());
+			URL url = uri.toURL();
+			System.out.println(url);
 			
+//			System.out.println("Scheme = " + uri.getScheme());
+//			System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+//			System.out.println("Authority = " + uri.getAuthority());
+//			System.out.println("User info = " + uri.getUserInfo());
+//			System.out.println("Host = " + uri.getHost());
+//			System.out.println("Port = " + uri.getPort());
+//			System.out.println("Path = " + uri.getPath());
+//			System.out.println("Query = " + uri.getQuery());
+//			System.out.println("Fragment = " + uri.getFragment());
+	
 		} catch (URISyntaxException e) {
 			System.out.println("Uri bad syntax: " + e.getMessage());
+		}catch (MalformedURLException e) {
+			System.out.println("Malformed url " + e.getMessage());
 		}
 	}
 	
-	/**
-	 * @URI_parts
-	 *  :Scheme = db
-	 *	:Scheme-specific part = //username:password@myserver.com:5000/catalogue/phone?os=android
-	 *	:Authority = username:password@myserver.com:5000
-	 *	:User info = username:password
-	 *	:Host = myserver.com
-	 *	:Port = 5000
-	 *	:Path = /catalogue/phone
-	 *	:Query = os=android
-	 *	:Fragment = samsung
-	 * */
 }
