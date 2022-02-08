ebc7d92529d018bc9b1e0b9c938ba50540a49a0d
benNjogu <kamandemachafi.gmail.com>
2022-02-08 11:35:53 +0300

    HTTPUrlConnection

--- Networking/src/com/keytech/Main.java
+++ Networking/src/com/keytech/Main.java
@@ -2,7 +2,8 @@ package com.keytech;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader; 
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -14,30 +15,44 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			URL url = new URL("http://example.org");
-			URLConnection urlConnection = url.openConnection();
-			urlConnection.setDoOutput(true);
-			urlConnection.connect();
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.addRequestProperty("User-Agent", "Chrome");
+			connection.setReadTimeout(30000);
 			
-			BufferedReader inputStream = new BufferedReader
-					(new InputStreamReader(urlConnection.getInputStream()));
-
-			Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
-			for(Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
-				String key = entry.getKey();
-				List<String> value = entry.getValue();
-				System.out.println("-------key = " + key);
-				for(String string : value) {
-					System.out.println("value = " + string);
-				}
+			int responseCode = connection.getResponseCode();
+			System.out.println("Response code = " + responseCode);
+			if (responseCode != 200) {
+				System.out.println("Error reading webpage");
+				return;
 			}
 			
-//			String line = "";
-//			while (line != null) {
-//				line = inputStream.readLine();
-//				System.out.println(line);
+			BufferedReader inputReader = new BufferedReader
+					(new InputStreamReader(connection.getInputStream()));
+			
+			String line;
+			while ((line = inputReader.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			inputReader.close();
+
+			
+//			urlConnection.setDoOutput(true);
+//			urlConnection.connect();
+//			
+
+//			Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+//			for(Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
+//				String key = entry.getKey();
+//				List<String> value = entry.getValue();
+//				System.out.println("-------key = " + key);
+//				for(String string : value) {
+//					System.out.println("value = " + string);
+//				}
 //			}
-//
-//			inputStream.close();
+//			
+			
 
 		} catch (IOException e) {
 			System.out.println("IOException " + e.getMessage());
