5cccc760581281e49b3810524841d98ae65fec85
benNjogu <kamandemachafi.gmail.com>
2022-02-08 09:47:42 +0300

    example.org

--- Networking/src/com/keytech/Main.java
+++ Networking/src/com/keytech/Main.java
@@ -9,30 +9,31 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-//			URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phone?os=android#samsung");
-			URI baseUri = new URI("http://username:password@mynewserver.com:5000");
-			URI uri1 = new URI("/catalogue/phone?os=android#samsung");
-			URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung");
-			URI uri3 = new URI("/stores/locations/?zip=12345");
+			URL url = new URL("http://example.org");
+			URI uri = url.toURI();
 			
-			URI resolvedUri1 = baseUri.resolve(uri1);
-			URI resolvedUri2 = baseUri.resolve(uri2);
-			URI resolvedUri3 = baseUri.resolve(uri3);
+			System.out.println("Scheme = " + uri.getScheme());
+			System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
+			System.out.println("Authority = " + uri.getAuthority());
+			System.out.println("User info = " + uri.getUserInfo());
+			System.out.println("Host = " + uri.getHost());
+			System.out.println("Port = " + uri.getPort());
+			System.out.println("Path = " + uri.getPath());
+			System.out.println("Query = " + uri.getQuery());
+			System.out.println("Fragment = " + uri.getFragment());
 			
-			URL url1 = resolvedUri1.toURL();
-			System.out.println("url 1 = " + url1);
-			//url 1 = http://username:password@mynewserver.com:5000/catalogue/phone?os=android#samsung
-			URL url2 = resolvedUri2.toURL();
-			System.out.println("url 2 = " + url2);
-			//url 2 = http://username:password@mynewserver.com:5000/catalogue/tvs?manufacturer=samsung
-			URL url3 = resolvedUri3.toURL();
-			System.out.println("url 3 = " + url3);
-			//url 3 = http://username:password@mynewserver.com:5000/stores/locations/?zip=12345
-			
-			URI relativizedUri = baseUri.relativize(resolvedUri2);
-			System.out.println("Relative URI = " + relativizedUri);
-			//Relative URI = catalogue/tvs?manufacturer=samsung
-	
+			/**
+			 * Scheme = http
+				Scheme-specific part = //example.org
+				Authority = example.org
+				User info = null
+				Host = example.org
+				Port = -1
+				Path = 
+				Query = null
+				Fragment = null
+			 * */
+				
 		} catch (URISyntaxException e) {
 			System.out.println("Uri bad syntax: " + e.getMessage());
 		}catch (MalformedURLException e) {
