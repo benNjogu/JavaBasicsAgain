b54c822556cf34e4a2c5dec2bc4e34fd54a8c0c3
benNjogu <kamandemachafi.gmail.com>
2022-02-08 10:22:43 +0300

    URL connections and inputStreamReader.

--- Networking/src/com/keytech/Main.java
+++ Networking/src/com/keytech/Main.java
@@ -4,14 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader; 
 import java.net.URL;
+import java.net.URLConnection;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
 			URL url = new URL("http://example.org");
+			URLConnection urlConnection = url.openConnection();
+			urlConnection.setDoOutput(true);
+			urlConnection.connect();
+			
 			BufferedReader inputStream = new BufferedReader
-					(new InputStreamReader(url.openStream()));
+					(new InputStreamReader(urlConnection.getInputStream()));
 
 			String line = "";
 			while (line != null) {
