d9ca83e4f6bd9b5da6ae26c3974a260b3c5c6c6d
benNjogu <kamandemachafi.gmail.com>
2022-02-08 10:05:26 +0300

    Using inputStreamReader.

--- Networking/src/com/keytech/Main.java
+++ Networking/src/com/keytech/Main.java
@@ -1,8 +1,8 @@
 package com.keytech;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader; 
 import java.net.URL;
 
 public class Main {
@@ -10,35 +10,20 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			URL url = new URL("http://example.org");
-			URI uri = url.toURI();
-			
-			System.out.println("Scheme = " + uri.getScheme());
-			System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-			System.out.println("Authority = " + uri.getAuthority());
-			System.out.println("User info = " + uri.getUserInfo());
-			System.out.println("Host = " + uri.getHost());
-			System.out.println("Port = " + uri.getPort());
-			System.out.println("Path = " + uri.getPath());
-			System.out.println("Query = " + uri.getQuery());
-			System.out.println("Fragment = " + uri.getFragment());
-			
-			/**
-			 * Scheme = http
-				Scheme-specific part = //example.org
-				Authority = example.org
-				User info = null
-				Host = example.org
-				Port = -1
-				Path = 
-				Query = null
-				Fragment = null
-			 * */
-				
-		} catch (URISyntaxException e) {
-			System.out.println("Uri bad syntax: " + e.getMessage());
-		}catch (MalformedURLException e) {
-			System.out.println("Malformed url " + e.getMessage());
+			BufferedReader inputStream = new BufferedReader
+					(new InputStreamReader(url.openStream()));
+
+			String line = "";
+			while (line != null) {
+				line = inputStream.readLine();
+				System.out.println(line);
+			}
+
+			inputStream.close();
+
+		} catch (IOException e) {
+			System.out.println("IOException " + e.getMessage());
 		}
 	}
-	
+
 }
