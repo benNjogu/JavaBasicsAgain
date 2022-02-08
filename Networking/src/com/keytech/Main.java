2931ece2a7bf22ec60357ae8e47fde83de7c1e26
benNjogu <kamandemachafi.gmail.com>
2022-02-08 08:55:56 +0300

    Using base uri with relative uri(s)

--- Networking/src/com/keytech/Main.java
+++ Networking/src/com/keytech/Main.java
@@ -10,21 +10,21 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 //			URI uri = new URI("http://username:password@myserver.com:5000/catalogue/phone?os=android#samsung");
-			URI baseUri = new URI("http://username:password@myserver.com:5000");
-			URI uri = new URI("/catalogue/phone?os=android#samsung");
-			URI resolvedUri = baseUri.resolve(uri);
-			URL url = resolvedUri.toURL();
-			System.out.println(url);
+			URI baseUri = new URI("http://username:password@mynewserver.com:5000");
+			URI uri1 = new URI("/catalogue/phone?os=android#samsung");
+			URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung");
+			URI uri3 = new URI("/stores/locations/?zip=12345");
 			
-//			System.out.println("Scheme = " + uri.getScheme());
-//			System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-//			System.out.println("Authority = " + uri.getAuthority());
-//			System.out.println("User info = " + uri.getUserInfo());
-//			System.out.println("Host = " + uri.getHost());
-//			System.out.println("Port = " + uri.getPort());
-//			System.out.println("Path = " + uri.getPath());
-//			System.out.println("Query = " + uri.getQuery());
-//			System.out.println("Fragment = " + uri.getFragment());
+			URI resolvedUri1 = baseUri.resolve(uri1);
+			URI resolvedUri2 = baseUri.resolve(uri2);
+			URI resolvedUri3 = baseUri.resolve(uri3);
+			
+			URL url1 = resolvedUri1.toURL();
+			System.out.println("url 1 = " + url1);
+			URL url2 = resolvedUri2.toURL();
+			System.out.println("url 2 = " + url2);
+			URL url3 = resolvedUri3.toURL();
+			System.out.println("url 3 = " + url3);
 	
 		} catch (URISyntaxException e) {
 			System.out.println("Uri bad syntax: " + e.getMessage());
